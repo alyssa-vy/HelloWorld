0ad22d6e2567eb3fbdaed2fa13f34398b67d17ae
alyssa-vy <56896185+alyssa-vy@users.noreply.github.com>
2020-05-28 10:19:25 -0700

    Initial Commit

---
+++ HelloWorld/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by Alyssa Vy on 5/28/20.
+//  Copyright © 2020 Alyssa Vy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
